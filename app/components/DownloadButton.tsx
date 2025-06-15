cacaebf035aed1085a81c87e14dc97e04c2f1df1
Dalvin Josias Sejour <dalvin.josias@gmail.com>
2025-06-15 00:54:14 -0400

    lets go

--- app/components/DownloadButton.tsx
+++ app/components/DownloadButton.tsx
@@ -10,7 +10,7 @@ export const DownloadButton = ({ url, text }: { url: string, text: string }) => 
       type="button"
       className="bg-green-500 hover:bg-green-600 text-white px-8 py-4 text-lg"
       onClick={() => {
-        window.open('https://apps.apple.com/us/app/learn-creole-with-jorge/id6745862015', '_blank', 'noopener,noreferrer');
+        window.open(url, '_blank', 'noopener,noreferrer');
       }}
     >
       <Apple className="mr-2 h-5 w-5" />
